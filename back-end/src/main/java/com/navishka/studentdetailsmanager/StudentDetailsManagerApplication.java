64645578c22cb3984b80a2b19357611ef345ecdb
navishkauoc <navishka.randika@gmail.com>
2022-06-17 19:47:14 +0530

    Spring Boot project add with Liquibase initial configs

---
+++ back-end/src/main/java/com/navishka/studentdetailsmanager/StudentDetailsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.navishka.studentdetailsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDetailsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDetailsManagerApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/navishka/studentdetailsmanager/StudentDetailsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navishka.studentdetailsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDetailsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
